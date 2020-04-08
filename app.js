fcb7ccf775d0bc8bb9a7beea1aee2fe1cd351083
stewajos <stewajos@mail.gvsu.edu>
2020-04-07 21:55:23 -0400

    displays cards correctly

--- app.js
+++ app.js
@@ -7,11 +7,37 @@ app.use('/js', express.static(path.join(__dirname, 'js')));
 app.engine('.html', require('ejs').renderFile);
 
 app.get('/', function (req, res) { //website link home page
-    request.get("https://api.magicthegathering.io/v1/types", function (error, response, responseBody) {
+    request.get("https://api.magicthegathering.io/v1/cards", function (error, response, responseBody) {
             if (error) {
               console.log(error);
             }
             var body = JSON.parse(responseBody);
+           // console.log(body.cards);
+            //console.log(typeof Object.keys(body.cards)[0]);
+
+            //delete the foreign names of the cards
+            for(var i =0; i< body.cards.length; i++){
+                // name, cmc, colors, colorIdentity, type, supertypes, types, subtypes, setName, toughness, loyalty, power, legality
+                console.log(body.cards[i]);
+                var temp = {};
+                temp.name = body.cards[i].name;
+                temp.cmc = body.cards[i].cmc;
+                temp.colors = body.cards[i].colors;
+                temp.colorIdentity = body.cards[i].colorIdentity;
+                temp.type = body.cards[i].type;
+                temp.supertypes = body.cards[i].supertypes;
+                temp.types = body.cards[i].types;
+                temp.subtypes = body.cards[i].subtypes;
+                temp.setName = body.cards[i].setName;
+                temp.toughness = body.cards[i].toughness;
+                temp.loyalty = body.cards[i].loyalty;
+                temp.power = body.cards[i].power;
+                temp.legality = body.cards[i].legality;
+                temp.multiverseid = body.cards[i].multiverseid;
+                temp.imageUrl = body.cards[i].imageUrl;
+                body.cards[i]= temp;
+            }
+
             body=JSON.stringify(body);
             res.render( __dirname + "/index.html", {body: body} );
           });
--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 function test(body){
-    console.log(body);
+    console.log(body.cards);
+    displayImage(body.cards[0]);
+}
+//document.getelementbyid
+//document is an object version of the html
+function displayImage(card){
+    var card1 = document.getElementById("card");
+    card1.innerHTML += '<img src="' + card.imageUrl + '"/>'
+    console.log(card1);
 }
